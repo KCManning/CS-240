0ba7dd25f73dcc0a3efcbbd462fdd0a116dd27f7
Kevin Manning <nachyochez@gmail.com>
2016-01-16 10:21:30 -0800

    Base interface connectivity

--- BetterCurve/app/src/main/java/manningk/bettercurve/MainScreen.java
+++ BetterCurve/app/src/main/java/manningk/bettercurve/MainScreen.java
@@ -2,6 +2,7 @@ package manningk.bettercurve;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -10,4 +11,29 @@ public class MainScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
     }
+
+    public void btnStatsScreenOnClick (View view)
+    {
+        setContentView(R.layout.activity_stats_screen);
+
+    }
+
+    public void btnBuildScreenOnClick (View view)
+    {
+        setContentView(R.layout.activity_build_screen);
+
+    }
+
+    public void btnsCancelOnClick (View view)
+    {
+        setContentView(R.layout.activity_main_screen);
+
+    }
+
+    public void btnsAddCardOnClick (View view)
+    {
+        setContentView(R.layout.activity_main_screen);
+
+    }
+
 }
