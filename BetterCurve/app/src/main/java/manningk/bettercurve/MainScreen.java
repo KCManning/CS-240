d299e1185ab0192120addd4a6a1b12018f57f909
Kevin Manning <nachyochez@gmail.com>
2016-01-19 17:04:33 -0800

    After class backup

--- BetterCurve/app/src/main/java/manningk/bettercurve/MainScreen.java
+++ BetterCurve/app/src/main/java/manningk/bettercurve/MainScreen.java
@@ -1,12 +1,13 @@
 package manningk.bettercurve;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainScreen extends AppCompatActivity {
-    Card testCard;
+
 
 
     @Override
@@ -19,30 +20,30 @@ public class MainScreen extends AppCompatActivity {
 
     public void btnBuildScreenOnClick (View view)
     {
-        setContentView(R.layout.activity_build_screen);
 
-        testCard = Card.getTestCard();
-
-        EditText txtName = (EditText) findViewById(R.id.txtName);
-        txtName.setText(testCard.getM_strName());
-
-        EditText txtCost = (EditText) findViewById(R.id.txtCost);
-        txtCost.setText(String.valueOf(testCard.getM_intCost()));
-
-        EditText txtQty = (EditText) findViewById(R.id.txtQty);
-        txtQty.setText(String.valueOf(1));
+        Intent intent = new Intent(this, BuildDeck.class);
+        //intent.putExtra() <-Adds info to be passed into the new activity, such as deck loading.
+        startActivity(intent);
 
     }
 
-    public void btnsCancelOnClick (View view)
+    /*public void btnsCancelOnClick (View view)
     {
         setContentView(R.layout.activity_main_screen);
 
-    }
+    }*/
 
     public void btnsAddCardOnClick (View view)
     {
         setContentView(R.layout.activity_main_screen);
+
+    }
+
+    public void btnStatsScreenOnClick (View view)
+    {
+        Intent intent = new Intent(this, DeckStats.class);
+        //intent.putExtra() <-Adds info to be passed into the new activity, such as deck loading.
+        startActivity(intent);
 
     }
 
