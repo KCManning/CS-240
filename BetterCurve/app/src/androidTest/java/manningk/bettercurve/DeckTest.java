d2776aff61dc47f82eec1cc35eef8ac752b5e217
Kevin Manning <nachyochez@gmail.com>
2016-01-31 22:25:56 -0800

    Hospital backup

---
+++ BetterCurve/app/src/androidTest/java/manningk/bettercurve/DeckTest.java
@@ -0,0 +1,84 @@
+package manningk.bettercurve;
+
+import android.test.AndroidTestCase;
+
+import java.util.List;
+
+/**
+ * Created by Kevin on 1/27/2016.
+ * Implementation based off of in-class project
+ */
+public class DeckTest extends AndroidTestCase {
+
+    Deck d;
+
+    public void setUp() throws Exception {
+        super.setUp();
+        d = Deck.getDeck();
+    }
+
+    public void testGetDeckList() throws Exception {
+        List<Card> list = d.getDeckList();
+        assertNotNull("getDeckList failed", list);
+    }
+
+    //Not yet implemented
+    /*
+        public void testValidGetCard() throws Exception {
+            int intStats[] = {3, 5};
+            String strStatNames[] = {"Defense", "Durability"};
+            Card card = new Card("Make Believe", "Fh", 002, "False Defense", 7, "Please Work",
+                    "I AM.", intStats, strStatNames);
+            d.addCard(card);
+            Card c = d.getCard(card.getCardID());
+            assertNotNull("getCard failed", c);
+        }
+*/
+    public void testInvalidGetCard() throws Exception {
+        Card c70 = d.getCard(70);
+        assertNull("getCard failed", c70);
+    }
+
+    public void testValidAddCard() throws Exception {
+        int size1 = d.getDeckList().size();
+        int intStats[] = {3, 5};
+        String strStatNames[] = {"Defense", "Durability"};
+        Card card = new Card("Make Believe", "Fh", 002, "False Defense", 7, "Please Work",
+                "I AM.", intStats, strStatNames);
+        int size2 = d.getDeckList().size();
+        assertTrue("addCard failed", size2 - size1 == 1);
+    }
+
+    //Not yet implemented
+    /*
+    public void testInvalidAddCard() throws Exception {
+        int size1 = d.getDeckList().size();
+        Card c1 = null;
+        d.addCard(c1);
+        int size2 = d.getDeckList().size();
+        assertTrue("addCard failed", size2 - size1 == 0);
+    }
+*/
+
+//Not yet implemented
+    /*
+    public void testValidDeleteCard() throws Exception {
+        int size1 = d.getDeckList().size();
+        d.deleteCard(0);
+        int size2 = d.getDeckList().size();
+        assertTrue("deleteCard failed", size1 - size2 == 1);
+    }
+*/
+
+//Not yet implemented
+/*
+    public void testInvalidDeleteCard() throws Exception {
+        int size1 = d.getDeckList().size();
+        d.deleteCard(10);
+        int size2 = d.getDeckList().size();
+        assertTrue("deleteCard failed", size1 - size2 == 0);
+    }
+    */
+
+
+}
