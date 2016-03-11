6fa0ca74991e67538e5e7745a99fc68f7bb3c8e4
Kevin Manning <nachyochez@gmail.com>
2016-03-11 12:22:56 -0800

    Database quasi functinal

--- BetterCurve/app/src/main/java/manningk/bettercurve/DataHelper.java
+++ BetterCurve/app/src/main/java/manningk/bettercurve/DataHelper.java
@@ -1,6 +1,7 @@
 package manningk.bettercurve;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -9,9 +10,9 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Array;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Vector;
 
 /**
  * This class provides intermediary layer to the database.
@@ -22,13 +23,14 @@ import java.util.Vector;
 public class DataHelper extends SQLiteOpenHelper {
 
     private static final String DB_NAME = "cardDB";
-    private static final int DB_VERSION = 1;
+    private static final int DB_VERSION = 2;
 
     /**
      * Constructor.
      */
     public DataHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
+        setCards(context);
     }
 
     /**
@@ -39,22 +41,22 @@ public class DataHelper extends SQLiteOpenHelper {
      */
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String sqlStr = "CREATE TABLE Sets (_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+        String sqlStr = "CREATE TABLE Sets (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + "SetCode TEXT, SetName TEXT)";
-       db.execSQL(sqlStr);
-        sqlStr = "CREATE TABLE Cards (_id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                + "FOREIGN KEY(SetCode) REFERENCES Sets(SetCode),"
-                + "CardNumber INTEGER, CardName TEXT, StatNames TEXT,"
-                + "CardStats TEXT)";
-       db.execSQL(sqlStr);
-        sqlStr = "CREATE TABLE DeckCards (_id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                + "FOREIGN KEY(DeckID) REFERENCES Cards(DeckID),"
-                + "FOREIGN KEY(CardID) REFERENCES Decks(CardIF),"
-                + "CardNumber INTEGER,)";
         db.execSQL(sqlStr);
-        sqlStr = "CREATE TABLE Decks (_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+        sqlStr = "CREATE TABLE Cards (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + "SetCode TEXT, FOREIGN KEY(SetCode) REFERENCES Sets(SetCode), "
+                + "CardNumber TEXT NOT NULL, CardName TEXT NOT NULL, CardCost INTEGER NOT NULL, "
+                + "StatNames TEXT NOT NULL, CardStats TEXT NOT NULL, Ability TEXT NOT NULL, Flavor TEXT NOT NULL)";
+        db.execSQL(sqlStr);
+        sqlStr = "CREATE TABLE DeckCards (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + "DeckID INTEGER, FOREIGN KEY(DeckID) REFERENCES Decks(_id), "
+                + "CardID INTEGER, FOREIGN KEY(CardID) REFERENCES Cards(_id), "
+                + "Quantity INTEGER)";
+        db.execSQL(sqlStr);
+        sqlStr = "CREATE TABLE Decks (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + "DeckName TEXT)";
-       db.execSQL(sqlStr);
+        db.execSQL(sqlStr);
     }
 
     /**
@@ -73,7 +75,7 @@ public class DataHelper extends SQLiteOpenHelper {
         Cursor results = null;
         try {
             db = this.getReadableDatabase();
-            results = db.rawQuery("SELECT * FROM Client", null);
+            results = db.rawQuery("SELECT * FROM Decks", null);
             if (results.moveToFirst()) {
                 do {
                     int id = (int) results.getLong(results.getColumnIndex("_id"));
@@ -141,29 +143,69 @@ public class DataHelper extends SQLiteOpenHelper {
     }
 
 
-    private void setCards(SQLiteDatabase db)
-    {
+    private void setCards(Context context) {
+        SQLiteDatabase db = null;
         try {
-            FileReader fileReader = new FileReader("Data.crd");
-            BufferedReader inputFile = new BufferedReader(fileReader);
+            db = this.getReadableDatabase();
+            AssetManager assetManager = context.getAssets();
+            InputStream input = assetManager.open("Data.crd");
+
+            BufferedReader inputFile = new BufferedReader(new InputStreamReader(input));
 
             String inputLines = inputFile.readLine();
             StringBuffer data = new StringBuffer();
+            String sqlStr = "INSERT INTO Sets (SetName, SetCode) VALUES (Crystals, SE)";
+            db.execSQL(sqlStr);
+            sqlStr = "INSERT INTO Sets (SetName, SetCode) VALUES (Dark Prophecies, DP)";
+            db.execSQL(sqlStr);
+            sqlStr = "INSERT INTO Cards (CardName, SetCode, CardNumber, CardCost, StatNames," +
+                    "CardStats, Ability, Flavor) VALUES (";
 
-            while (inputLines != null)
-            {
-                data.append(inputLines + "\n");
+            while (inputLines != null) {
+                //inputLines = inputLines.replace("\t", "\n");
+                /*
+                Name
+                Set
+                Number
+                Cost
+                Stats1
+                Stats2
+                Ability
+                Flavor
+                 */
+                String name = inputLines.substring(0, inputLines.indexOf("\t"));
+                inputLines = inputLines.replaceFirst(name + "\t", "");
+                String set = inputLines.substring(0, inputLines.indexOf("\t"));
+                inputLines = inputLines.replaceFirst(set + "\t", "");
+                //set = db.rawQuery("SELECT SetCode FROM Sets WHERE SetName = " + set, null).toString();
+                String number = inputLines.substring(0, inputLines.indexOf("\t"));
+                inputLines = inputLines.replaceFirst(number + "\t", "");
+                String cost = inputLines.substring(0, inputLines.indexOf("\t"));
+                inputLines = inputLines.replaceFirst(cost + "\t", "");
+                String statNames = inputLines.substring(0, inputLines.indexOf("\t"));
+                inputLines = inputLines.replaceFirst(statNames + "\t", "");
+                String stats = inputLines.substring(0, inputLines.indexOf("\t"));
+                inputLines = inputLines.replaceFirst(stats + "\t", "");
+                String ability = inputLines.substring(0, inputLines.indexOf("\t"));
+                inputLines = inputLines.replaceFirst(ability + "\t", "");
+                String flavor = inputLines;
+                sqlStr += "\"" + name + "\"," + "\"" + set + "\"," + "\"" + number + "\","
+                        + "\"" + cost + "\"," + "\"" + statNames + "\","
+                        + "\"" + stats + "\"," + "\"" + ability + "\"," + "\"" + flavor + "\")";
+
+                db.execSQL(sqlStr);
                 inputLines = inputFile.readLine();
+
             }
 
-            String sqlStr = "INSERT INTO TABLE Cards (SetCode, )"
-                    +"VALUES" +
-                    "(_id INTEGER PRIMARY KEY AUTOINCREMENT, SetCode TEXT, SetName TEXT)";
-            db.execSQL(sqlStr);
+            input.close();
+
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
